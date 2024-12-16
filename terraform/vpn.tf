0e8406e59688097b26af81ae1c5473f5d4367e0d
Rich Robinson <richard.robinson@deltarail.com>
2024-12-16 20:06:23 +0000

    Skeleton structure for AWS client VPN configuration

---
+++ terraform/vpn.tf
@@ -0,0 +1,20 @@
+##################################################################################
+# CONFIGURATION FOR AWS CLIENT VPN CONNECTION
+##################################################################################
+
+# Create some certificates (external pre-requisite)
+
+# Add certificate for VPN server host to AWS Certificate Manager
+
+# Validate the certificate
+
+# Add root certificate to AWS Certificate Manager
+
+# Create Security Group for VPN connections
+
+# Create client VPN endpoint
+
+# Create client VPN endpoint attachment(s)
+
+# Create client VPN authorisation rule
+
